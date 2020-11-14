310bf442fd2de0493a76938d5e0ab61c24816d22
Martin Afani <martin@orderly.ai>
2020-11-14 20:17:26 +0300

    Implement the auth_respository

---
+++ lib/repositories/auth/auth_repository.dart
@@ -0,0 +1,88 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_journal_app/constants/paths.dart';
+import 'package:flutter_journal_app/entities/user/user_entity.dart';
+import 'package:flutter_journal_app/models/user/user_model.dart';
+import 'package:flutter_journal_app/repositories/auth/i_auth_repository.dart';
+
+class AuthRepository extends IAuthRepository {
+  final FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firebaseFirestore;
+
+  AuthRepository(
+      {@required FirebaseAuth firebaseAuth,
+      @required FirebaseFirestore firebaseFirestore})
+      : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+        _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+
+  Future<UserModel> _firebaseUserToUser(User firebaseUser) async {
+    final userDoc = await _firebaseFirestore
+        .collection(Paths.users)
+        .doc(firebaseUser.uid)
+        .get();
+    if (userDoc.exists) {
+      final user = UserModel.fromEntity(UserEntity.fromDocument(userDoc));
+      return user;
+    }
+
+    return UserModel(id: firebaseUser.uid, email: '');
+  }
+
+  @override
+  Future<UserModel> loginAnonymously() async {
+    final authResult = await _firebaseAuth.signInAnonymously();
+    return await _firebaseUserToUser(authResult.user);
+  }
+
+  @override
+  Future<UserModel> loginWithEmailAndPassword(
+      String email, String password) async {
+    final authResult = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return await _firebaseUserToUser(authResult.user);
+  }
+
+  @override
+  Future<UserModel> signUpWithEmailAndPassword(
+      String email, String password) async {
+    final currentUser = _firebaseAuth.currentUser;
+    // Link the current anonymous user with an email and password
+    // account when they sign up
+    final authCredential =
+        EmailAuthProvider.credential(email: email, password: password);
+    // Link the two accounts
+    final authResult = await currentUser.linkWithCredential(authCredential);
+    final user = await _firebaseUserToUser(authResult.user);
+    final userModel = UserModel(email: user.email, id: user.id);
+
+    await _firebaseFirestore
+        .collection(Paths.users)
+        .doc(user.id)
+        .set(userModel.toEntity().toDocument());
+    return user;
+  }
+
+  @override
+  Future<UserModel> logout() async {
+    await _firebaseAuth.signOut();
+
+    return loginAnonymously();
+  }
+
+  @override
+  Future<UserModel> getCurrentUser() async {
+    final currentUser = _firebaseAuth.currentUser;
+    if (currentUser == null) {
+      return null;
+    }
+    final user = await _firebaseUserToUser(currentUser);
+    return user;
+  }
+
+  @override
+  Future<bool> isAnonymous() async {
+    final currentUser = _firebaseAuth.currentUser;
+    return currentUser.isAnonymous;
+  }
+}
---
+++ lib/repositories/auth/i_auth_repository.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_journal_app/models/user/user_model.dart';
+
+abstract class IAuthRepository {
+  Future<UserModel> loginAnonymously();
+  Future<UserModel> loginWithEmailAndPassword(String email, String password);
+  Future<UserModel> signUpWithEmailAndPassword(String email, String password);
+  Future<UserModel> logout();
+  Future<bool> isAnonymous();
+  Future<UserModel> getCurrentUser();
+}
